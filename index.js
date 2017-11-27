767c32a8f41d80ac3d6442dd5b99787910c617e4
Eric Bearfield <embearfield@gmail.com>
2017-11-27 18:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) { 
     return "I can't hear you!";
   }
-    else if (string === "HELLO") {
+    else if (string.toUpperCase() === string) {
     return "YES INDEED!";
     }
     else if (string === 'I love you, Grandma.') { 
